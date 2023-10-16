e4154a179b7f821bc84024fbef4c77b12edb027b
Expedito <expeditopaivaneto@gmail.com>
2023-10-16 15:26:07 -0300

    Criando componente de lista de pokemons e add ele e search em home

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,22 @@ import { CommonModule } from '@angular/common';
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { SearchComponent } from './search/search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    SearchComponent
+    SearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    SearchComponent,
+    PokeListComponent
   ]
 })
 export class SharedModule { }
