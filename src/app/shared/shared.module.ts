548c66f568a6b27cb8b09dff687ca495abebfbaf
Expedito <expeditopaivaneto@gmail.com>
2023-10-16 15:20:08 -0300

    Criando componente de search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
